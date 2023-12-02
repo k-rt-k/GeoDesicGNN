9ec6e6fec524a4be0a003b92b8428c501f5de9b0
k-rt-k <94541845+k-rt-k@users.noreply.github.com>
2023-12-02 16:22:15 +0530

    gratt

--- gnnfree/nn/pooling.py
+++ gnnfree/nn/pooling.py
@@ -120,33 +120,104 @@ class GDTransform(Transform):
         )
         return node_repr
 
-# class GraphAttentionPooler(Pooler):
-#     def __init__(self, emb_dim, num_heads=1, dropout=0.0):
-#         super().__init__()
-#         self.emb_dim = emb_dim
-#         self.num_heads = num_heads
-#         self.dropout = dropout
-#         self.att = nn.MultiheadAttention(
-#             emb_dim, num_heads, dropout=dropout
-#         )
-#         self.mlp = MLPLayers(2, h_units=[emb_dim, 2 * emb_dim, emb_dim])
+class GDAttnTransform(Transform):
+    """compute node-level GD representation."""
 
-#     def forward(self, repr, ind, ind_block):
-#         """Compute graph-level representation.
+    def __init__(self, emb_dim, gd_deg=True, batch_norm=True) -> None:
+        super().__init__(emb_dim)
+        self.gd_deg = gd_deg    
+        if gd_deg:
+            self.mlp_combine_gd_deg = MLPLayers(
+                2,
+                h_units=[emb_dim + 1, 2 * emb_dim, emb_dim],
+                batch_norm=False,
+            )
+        self.mlp_combine_nei_gd = MLPLayers(
+            2,
+            h_units=[2 * emb_dim + 1, 4 * emb_dim, emb_dim],
+            batch_norm=batch_norm,
+        )
+        self.mlp_combine_node_nei = MLPLayers(
+            2,
+            h_units=[2 * emb_dim, 4 * emb_dim, emb_dim],
+            batch_norm=batch_norm,
+        )
+        ## one attention like mechanism, like qkv
+        self.Q = nn.Linear(self.emb_dim,self.emb_dim)
+        self.K = nn.Linear(self.emb_dim,self.emb_dim)
+        self.V = nn.Linear(self.emb_dim,self.emb_dim)
 
-#         Arguments:
-#             repr {torch.tensor} -- node representations
-#             ind {torch.tensor} -- graph node indices
-#             ind_block {torch.tensor} -- graph node block indices
+    def forward(
+        self,
+        repr,
+        nodes,
+        neighbors,
+        neighbor_count,
+        dist,
+        gd,
+        gd_count,
+        gd_deg,
+    ):
+        """
 
-#         Returns:
-#             torch.tensor -- graph-level representation
-#         """
-#         graph_repr = self.att(
-#             repr[ind].unsqueeze(1), repr[ind_block].unsqueeze(1)
-#         )[0].squeeze(1)
-#         graph_repr = self.mlp(graph_repr)
-#         return graph_repr
+        Arguments:
+            repr {torch.tensor} -- N*d tensor of node representations
+            nodes {torch.tensor} -- 1-d nodes of interests
+            neighbors {torch.tensor} -- neighbors indices of nodes
+            neighbor_count {torch.tensor} -- nodes[i]'s neighbor=
+            neighbors[cumsum(neighbor_count[:i]):cumsum(neighbor_count[:i+1])]
+            dist {torch.tensor} -- dist[i]=distance between neighbors[i] and
+            its corresponding source in nodes.
+            gd {torch.tensor} -- Vertical Geodesics indices of neighbors
+            gd_count {torch.tensor} -- neighbors[i]'s vertical geodesics=
+                    gd[cumsum(gd_count[i]):cumsum(gd_count[i+1])]
+            gd_deg {torch.tensor} -- same shape as gd, vertical gd degrees.
+
+        Returns:
+            Vertical geodesics of each node in nodes.
+        """
+        neighbors_repr = repr[neighbors]
+        gd_repr = repr[gd]
+        if self.gd_deg:
+            gd_repr = self.mlp_combine_gd_deg(
+                torch.cat([gd_repr, gd_deg.view(-1, 1)], dim=-1)
+            )
+        og_nodes = torch.repeat_interleave(repr,gd_len)
+        attnwts = torch.sum(self.Q(og_nodes)*self.K(gd_repr),dim=1)/torch.sqrt(self.emb_dim)
+        attnwts = F.sigmoid(attnwts)
+        gd_repr = self.V(gd_repr)
+        ### embs MUST be in the correct order
+        combined_gd_repr = scatter(
+            gd_repr,
+            count_to_group_index(gd_len),
+            dim=0,
+            dim_size=len(gd_len),
+            weight= attnwts,
+            reduce='mean'
+        )
+        
+        combined_gd_repr = scatter(
+            combined_gd_repr,
+            count_to_group_index(gd_count),
+            dim=0,
+            dim_size=len(gd_count),
+        )
+        combined_repr = self.mlp_combine_nei_gd(
+            torch.cat(
+                [combined_gd_repr, neighbors_repr, dist.view(-1, 1)], dim=-1
+            )
+        )
+        combined_repr = scatter(
+            combined_repr,
+            count_to_group_index(neighbor_count),
+            dim=0,
+            dim_size=len(neighbor_count),
+        )
+
+        node_repr = self.mlp_combine_node_nei(
+            torch.cat([combined_repr, repr[nodes]], dim=-1)
+        )
+        return node_repr
 
 class ReprIndexTransform(Transform):
     def forward(self, repr, ind):
@@ -271,11 +342,11 @@ class VerGDAttnTransform(VerGDTransform):
         ### embs MUST be in the correct order
         
         gd_repr = scatter(
-            attnwts,
+            gd_repr,
             count_to_group_index(gd_len),
             dim=0,
             dim_size=len(gd_len),
-            weight=gd_repr,
+            weight= attnwts,
             reduce='mean'
         )
         return gd_repr
--- models/graph_classifier.py
+++ models/graph_classifier.py
@@ -29,3 +29,28 @@ class GDGraphClassifier(BaseGraphEncoder):
 
     def get_out_dim(self):
         return self.emb_dim
+
+class GDGraphAttnClassifier(BaseGraphEncoder):
+    def __init__(self, emb_dim, gnn, gd_deg=True, batch_norm=True):
+        super().__init__(emb_dim, gnn)
+
+        self.gd_pool = GDAttnTransform(emb_dim, gd_deg, batch_norm)
+
+    def pool_from_graph(self, g, repr, input):
+        node_repr = self.gd_pool(
+            repr,
+            torch.arange(len(repr), device=repr.device),
+            input.neighbors,
+            input.neighbors_count,
+            input.dist,
+            input.gd,
+            input.gd_count,
+            input.gd_deg,
+        )
+        g.ndata["node_res"] = node_repr
+
+        g_sum = dgl.sum_nodes(g, "node_res")
+        return g_sum
+
+    def get_out_dim(self):
+        return self.emb_dim
